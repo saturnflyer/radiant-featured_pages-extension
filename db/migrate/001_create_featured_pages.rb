103dd2db4fa28f9a6ab9478d61ae82111f6b3843
Jim Gay <jim@saturnflyer.com>
2010-08-09 13:04:54 -0700

    reset column info after adding field

--- db/migrate/001_create_featured_pages.rb
+++ db/migrate/001_create_featured_pages.rb
@@ -1,6 +1,7 @@
 class CreateFeaturedPages < ActiveRecord::Migration
   def self.up
     add_column :pages, :featured_page, :boolean, :default => false
+    Page.reset_column_information
     Page.update_all(['featured_page = ?',false])
   end
 
