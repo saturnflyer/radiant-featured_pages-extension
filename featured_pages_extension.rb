da459b2265e50b4833a9ca1e511ccff02479105d
Jim Gay <jim@saturnflyer.com>
2010-11-06 00:43:13 -0400

    add featured date to edit form

--- featured_pages_extension.rb
+++ featured_pages_extension.rb
@@ -1,6 +1,6 @@
 class FeaturedPagesExtension < Radiant::Extension
   version "2"
-  description "Adds featured_page field to all pages to allow listing of pages marked regardless of their nesting within parent pages."
+  description "Adds featured_date field to all pages to allow listing of pages marked regardless of their nesting within parent pages."
   url "http://www.saturnflyer.com/"
   
   def activate
@@ -8,7 +8,7 @@ class FeaturedPagesExtension < Radiant::Extension
       include FeaturedPagesTags
       include PageExtensions
     }
-    admin.page.edit.add :form, "featured_page_meta"
+    admin.page.edit.add :layout, "featured_page_meta"
     if admin.respond_to?(:dashboard)
       admin.dashboard.index.add :extensions, 'featured_pages'
     end
