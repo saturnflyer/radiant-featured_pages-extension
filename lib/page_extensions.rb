5b9faf2b0f4cc87c15f87b3e22822ec1764a7d0e
Jim Gay <jim@saturnflyer.com>
2010-11-07 16:16:08 -0500

    don't need the blank string check

--- lib/page_extensions.rb
+++ lib/page_extensions.rb
@@ -1,7 +1,7 @@
 module PageExtensions
   def self.included(base)
     base.class_eval {
-      named_scope :featured, {:conditions => ["featured_date is NOT NULL or featured_date != ''"]}
+      named_scope :featured, {:conditions => ["featured_date is NOT NULL"]}
     }
   end
 end
